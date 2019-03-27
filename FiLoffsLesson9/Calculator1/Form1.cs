a645b8770c08f1a8819d5d5045941872572bba8d
FiLoffs <filod72@gmail.com>
2019-03-27 07:43:37 +0200

    состояние по лекции

---
+++ FiLoffsLesson9/Calculator1/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorGUI
+{
+    public partial class Calculator : Form
+    {
+        CalculatorFormula calculatorFormula;
+        bool isSecondNumber;
+
+        public Calculator()
+        {
+            InitializeComponent();
+            calculatorFormula = new CalculatorFormula();
+            isSecondNumber = false;
+        }
+
+        public void AddDigit(String digit)
+        {
+            if (display.Text.Equals("0") || isSecondNumber)
+                display.Text = digit;
+            else
+                display.Text += digit;
+
+            isSecondNumber = false;          
+            
+        }
+
+        public void ChooseOperation(object sender, EventArgs e)
+        {
+
+            switch (((Button)sender).Text)
+            {
+                case "+":
+                    calculatorFormula.OperationCode = 1;
+                    break;
+                case "-":
+                    calculatorFormula.OperationCode = 2;
+                    break;
+                case "*":
+                    calculatorFormula.OperationCode = 3;
+                    break;
+                case "/":
+                    calculatorFormula.OperationCode = 4;
+                    break;
+
+                default:
+                    break;
+            }
+
+            calculatorFormula.Number1 = Convert.ToDouble(display.Text);
+            isSecondNumber = true;
+        }
+
+        private void buttonDigit_Click(object sender, EventArgs e)
+        {
+            AddDigit(((Button)sender).Text);
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            double number = Convert.ToDouble(display.Text) * (-1);
+            display.Text = number.ToString();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (!display.Text.Contains(","))
+                display.Text += ",";
+            //display.Text = display.Text.Replace('.', ','); //- замена точек на запятые при вводе с клавииатуры
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            display.Text = "0";
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            calculatorFormula.Number2 = Convert.ToDouble(display.Text);
+            calculatorFormula.Calculate();
+            display.Text = calculatorFormula.Result;
+            isSecondNumber = true; //после нажатия равно - следующее число перезаписывает дисплей
+        }
+
+        private void buttonMemoryClear_Click(object sender, EventArgs e)
+        {
+            calculatorFormula.Memory = 0;
+            labelMemory.Visible = false;
+        }
+
+        private void buttonMemoryRead_Click(object sender, EventArgs e)
+        {
+            display.Text = calculatorFormula.Memory.ToString();
+            isSecondNumber = true;
+        }
+
+        private void buttonMemorySend_Click(object sender, EventArgs e)
+        {
+            calculatorFormula.Memory = Convert.ToDouble(display.Text);
+            isSecondNumber = true;
+            if (calculatorFormula.Memory != 0)
+                labelMemory.Visible = true;
+            else
+                labelMemory.Visible = false;
+        }
+
+        private void buttonMemoryPlus_Click(object sender, EventArgs e)
+        {
+            calculatorFormula.Memory += Convert.ToDouble(display.Text);
+            isSecondNumber = true;
+            if (calculatorFormula.Memory != 0)
+                labelMemory.Visible = true;
+            else
+                labelMemory.Visible = false;
+        }
+
+        private void buttonMemoryMinus_Click(object sender, EventArgs e)
+        {
+            calculatorFormula.Memory -= Convert.ToDouble(display.Text);
+            isSecondNumber = true;
+            if (calculatorFormula.Memory != 0)
+                labelMemory.Visible = true;
+            else
+                labelMemory.Visible = false;
+        }
+    }
+}
---
+++ FiLoffsLesson9/CalculatorFormula1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorGUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorFormula calculatorFormula = new CalculatorFormula();
+
+            calculatorFormula.OperationCode = 4;
+            calculatorFormula.Number1 = 3;
+            calculatorFormula.Number2 = 5;
+            calculatorFormula.Calculate();
+            Console.WriteLine(calculatorFormula.Result);
+        }
+    }
+}
